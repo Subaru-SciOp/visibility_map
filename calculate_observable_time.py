74976263210c02a09252b6207158db5e2197675d
Masato Onodera <masato.onodera@gmail.com>
2024-11-02 00:02:40 -1000

    done

--- calculate_observable_time.py
+++ calculate_observable_time.py
@@ -6,7 +6,7 @@ import sys
 import time
 import warnings
 from contextlib import redirect_stderr
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 import healpy as hp
 import joblib
@@ -22,6 +22,7 @@ from astroplan import (
 )
 from astropy import units as u
 from astropy.coordinates import SkyCoord
+from astropy.table import Table
 from astropy.time import Time
 from dotenv import load_dotenv
 
@@ -32,8 +33,9 @@ load_dotenv()
 
 # set the default values for the environment variables
 OBSERVATION_SITE = os.getenv("OBSERVATION_SITE", "subaru")
+UTC_OFFSET = float(os.getenv("UTC_OFFSET", -10))
 DELTA_TIME = float(os.getenv("DELTA_TIME", 5))
-HSC_DOMEOPEN_OFFSET = float(os.getenv("HSC_DOMEOPEN_OFFSET", 30))
+DOMEOPEN_OFFSET = float(os.getenv("DOMEOPEN_OFFSET", 30))
 MIN_ALTITUDE = float(os.getenv("MIN_ALTITUDE", 30))
 MAX_ALTITUDE = float(os.getenv("MAX_ALTITUDE", 90))
 N_JOBS = float(os.getenv("N_JOBS", 8))
@@ -41,11 +43,14 @@ N_JOBS = float(os.getenv("N_JOBS", 8))
 # the location of the observatory
 observer = Observer.at_site(OBSERVATION_SITE)
 
+# UTC offset for the observatory
+utc_offset = UTC_OFFSET * u.hour
+
 # time interval for calculation
 delta_time = DELTA_TIME * u.minute
 
 # offset for dome open time
-hsc_domeopen_offset = HSC_DOMEOPEN_OFFSET * u.minute
+domeopen_offset = DOMEOPEN_OFFSET * u.minute
 
 # min and max altitude for the object
 min_alt, max_alt = MIN_ALTITUDE * u.deg, MAX_ALTITUDE * u.deg
@@ -53,7 +58,8 @@ min_alt, max_alt = MIN_ALTITUDE * u.deg, MAX_ALTITUDE * u.deg
 
 # Function to calculate sunset and sunrise
 def calculate_sun_times(date, observer):
-    time = Time(date)
+    # assume date is local time and convert to UTC by adding the UTC offset
+    time = Time(date) + utc_offset
     sunset = observer.sun_set_time(time)
     sunrise = observer.sun_rise_time(time, which="next")
     return sunset, sunrise
@@ -63,9 +69,15 @@ def calculate_sun_times(date, observer):
 def compute_constraints(
     target,
     times,
-    max_moon_illumination: float = None,
-    min_moon_separation: float = None,
+    min_moon_illumination=None,
+    max_moon_illumination=None,
+    min_moon_separation: float = 30.0,
 ):
+    if min_moon_illumination is None:
+        min_moon_illumination = [None, 0.25, 0.65]
+    if max_moon_illumination is None:
+        max_moon_illumination = [0.25, 0.65, None]
+
     # compute the altitude constraint (true if the object is between the altitude limits)
     flag_obj_alt = AltitudeConstraint(min=min_alt, max=max_alt).compute_constraint(
         times, observer, target
@@ -87,53 +99,98 @@ def compute_constraints(
 
     # moon rise and set conditions are considered in the astroplan.MoonIlluminationConstraint class
     # true if the moon illumination is less than max_moon_illumination
-    flag_moon_illum = MoonIlluminationConstraint(
-        max=max_moon_illumination
-    ).compute_constraint(times, observer, target)
+    flag_moon_illum = []
+    for min_illum, max_illum in zip(min_moon_illumination, max_moon_illumination):
+        flag_moon_illum.append(
+            MoonIlluminationConstraint(min=min_illum, max=max_illum).compute_constraint(
+                times, observer, target
+            )
+        )
 
-    return flag_moon_sep, flag_moon_illum, flag_obj_alt
+    return flag_obj_alt, flag_moon_sep, flag_moon_illum
 
 
 def calculate_observable_time(
     targets,
     times,
-    max_moon_illumination: float = None,
+    min_moon_illumination=None,
+    max_moon_illumination=None,
     min_moon_separation: float = None,
 ):
+    if min_moon_illumination is None:
+        min_moon_illumination = [None, 0.25, 0.65]
+    if max_moon_illumination is None:
+        max_moon_illumination = [0.25, 0.65, None]
+
     # define a function to compute the observable time for a single target with joblib
     def _compute_constraints_to_joblib(target):
-        flag_moon_sep, flag_moon_illum, flag_obj_alt = compute_constraints(
+        flag_obj_alt, flag_moon_sep, flag_moon_illum = compute_constraints(
             target,
             times,
+            min_moon_illumination=min_moon_illumination,
             max_moon_illumination=max_moon_illumination,
             min_moon_separation=min_moon_separation,
         )
+
         # when returned early because of no visibility, return 0.
         if flag_moon_sep is None:
-            return 0.0
+            return [0.0, 0.0, 0.0]
 
         # combine the constraints
-        flag_observable = flag_moon_illum & flag_moon_sep & flag_obj_alt
+        flag_observable_dark = flag_moon_illum[0] & flag_moon_sep & flag_obj_alt
+        flag_observable_gray = flag_moon_illum[1] & flag_moon_sep & flag_obj_alt
+        flag_observable_bright = flag_moon_illum[2] & flag_moon_sep & flag_obj_alt
+
+        # output list
+        delta_t = delta_time.to(u.minute).value
+        res = [
+            float(len(times[flag_observable_dark]) * delta_t),
+            float(len(times[flag_observable_gray]) * delta_t),
+            float(len(times[flag_observable_bright]) * delta_t),
+        ]
 
         # return the total time the object is observable
-        return len(times[flag_observable]) * delta_time.to(u.minute).value
+        return res
 
-    # parallel calculation with joblib
+    # parallel calculation with joblib, parallelized over the targets, i.e., (ra, dec)
     parallel = joblib.Parallel(n_jobs=N_JOBS, return_as="generator")
-    output_observable_time = parallel(
+    output_joblib_observable_time = parallel(
         joblib.delayed(_compute_constraints_to_joblib)(target) for target in targets
     )
+    df_observable_time = pd.DataFrame(
+        {
+            "dark": np.zeros(len(targets)),
+            "gray": np.zeros(len(targets)),
+            "bright": np.zeros(len(targets)),
+        }
+    )
 
-    return np.array(list(output_observable_time), dtype=float)
+    for i, t in enumerate(list(output_joblib_observable_time)):
+        # print(t)
+        df_observable_time["dark"][i] += t[0]
+        df_observable_time["gray"][i] += t[1]
+        df_observable_time["bright"][i] += t[2]
+
+    # print(df_observable_time)
+
+    # print(len(observable_time))
+
+    return df_observable_time
 
 
 def main_calculate_observable_time(
-    df: pd.DataFrame,
+    dates,
     ra,
     dec,
-    max_moon_illumination: float = 0.25,
+    min_moon_illumination=None,
+    max_moon_illumination=None,
     min_moon_separation: float = 30,
 ):
+
+    if min_moon_illumination is None:
+        min_moon_illumination = [None, 0.25, 0.65]
+    if max_moon_illumination is None:
+        max_moon_illumination = [0.25, 0.65, None]
 
     t_start = time.time()
 
@@ -144,66 +201,84 @@ def main_calculate_observable_time(
     ]
 
     # array to store the observable time for each healpix pixel
-    time_observable = np.zeros(ra.size) * u.minute
+    date_out = np.empty((dates.size, ra.size), dtype=type(dates[0]))
+    ipix_out = np.zeros((dates.size, ra.size), dtype=int)
+    ra_out = np.zeros((dates.size, ra.size))
+    dec_out = np.zeros((dates.size, dec.size))
+    time_observable_dark = np.zeros((dates.size, ra.size))
+    time_observable_gray = np.zeros((dates.size, ra.size))
+    time_observable_bright = np.zeros((dates.size, ra.size))
 
     # loop over observing runs
-    for i in range(df.index.size):
-        print(f"Start: run from {df['run_begin'][i]} to {df['run_end'][i]}")
+    for i in range(dates.size):
+        t_date_start = time.time()
 
-        t_run_start = time.time()
+        date = dates[i]
 
-        current_date = df["run_begin"][i]
+        date_out[i, :] = [date.strftime("%Y-%m-%d")] * ra.size
 
-        while current_date <= df["run_end"][i]:
-            print(f"Start: night calculation for {current_date}")
-            t_night_start = time.time()
+        ipix_out[i, :] = np.arange(ra.size)
+        ra_out[i, :] = ra
+        dec_out[i, :] = dec
 
-            sunset, sunrise = calculate_sun_times(current_date, observer)
+        print(f"Start: night calculation for {date.strftime('%Y-%m-%d')}")
 
-            times = time_grid_from_range(
-                [sunset + hsc_domeopen_offset, sunrise - hsc_domeopen_offset],
-                time_resolution=delta_time,
-            )
+        sunset, sunrise = calculate_sun_times(date, observer)
 
-            # calculate the observable time for each target (array with the length of the number of targets)
-            res = calculate_observable_time(
-                targets,
-                times,
-                max_moon_illumination=max_moon_illumination,
-                min_moon_separation=min_moon_separation,
-            )
+        times = time_grid_from_range(
+            [sunset + domeopen_offset, sunrise - domeopen_offset],
+            time_resolution=delta_time,
+        )
 
-            time_observable += res * u.minute
+        # calculate the observable time for each target (array with the length of the number of targets (ra, dec))
+        df_obs_time_date = calculate_observable_time(
+            targets,
+            times,
+            min_moon_illumination=min_moon_illumination,
+            max_moon_illumination=max_moon_illumination,
+            min_moon_separation=min_moon_separation,
+        )
 
-            t_night_end = time.time()
+        time_observable_dark[i, :] = df_obs_time_date["dark"]
+        time_observable_gray[i, :] = df_obs_time_date["gray"]
+        time_observable_bright[i, :] = df_obs_time_date["bright"]
 
-            print(
-                f"Finished: night calculation for {current_date} ({t_night_end - t_night_start:.2f} seconds)"
-            )
-            current_date += timedelta(days=1)
-
-        t_run_end = time.time()
+        t_date_end = time.time()
         print(
-            f"Finished: run from {df['run_begin'][i]} to {df['run_end'][i]} ({t_run_end - t_run_start:.2f} seconds)"
+            f"Finished: night calculation for {date.strftime('%Y-%m-%d')} ({t_date_end - t_date_start:.2f} seconds)"
         )
 
     t_end = time.time()
 
     print(f"Calculation took {t_end - t_start:.2f} seconds")
 
-    return time_observable.to(u.hour).value
+    tbret = Table(
+        {
+            "date": date_out.flatten(),
+            "ipix": ipix_out.flatten(),
+            "ra": ra_out.flatten() * u.deg,
+            "dec": dec_out.flatten() * u.deg,
+            "dark": (time_observable_dark.flatten() * u.minute).to(u.hour),
+            "gray": (time_observable_gray.flatten() * u.minute).to(u.hour),
+            "bright": (time_observable_bright.flatten() * u.minute).to(u.hour),
+        }
+    )
+    return tbret
 
 
 def main(
-    observing_runs: str,
+    start_date: str,
+    end_date: str,
     outfile: str,
     nside: int,
+    min_moon_illumination: float = None,
     max_moon_illumination: float = 0.25,
     min_moon_separation: float = 30,
 ):
-    # Read observing_runs.csv
-    df_runs = pd.read_csv(observing_runs, parse_dates=["run_begin", "run_end"])
+    # create a list of dates for the observing runs
+    dates = pd.date_range(start=start_date, end=end_date, freq="1D", inclusive="both")
 
+    # healpix arrays
     nside = args.nside
     npix = hp.nside2npix(nside)
 
@@ -213,34 +288,47 @@ def main(
     # Generate ra, dec for each healpix pixel
     ra, dec = hp.pix2ang(nside, np.arange(npix), lonlat=True, nest=False)
 
-    observable_times = main_calculate_observable_time(
-        df_runs,
+    tbout = main_calculate_observable_time(
+        dates,
         ra,
         dec,
+        min_moon_illumination=min_moon_illumination,
         max_moon_illumination=max_moon_illumination,
         min_moon_separation=min_moon_separation,
     )
 
-    # create a pandas dataframe to store the results
-    dfout = pd.DataFrame(
-        {
-            "ra": ra,
-            "dec": dec,
-            "time_observable": observable_times,
-        }
-    )
+    tbout.meta["site"] = OBSERVATION_SITE
+    tbout.meta["nside"] = nside
+    tbout.meta["date_start"] = start_date
+    tbout.meta["date_end"] = end_date
+    tbout.meta["delta_time"] = DELTA_TIME
+    tbout.meta["min_moon_illumination"] = min_moon_illumination
+    tbout.meta["max_moon_illumination"] = max_moon_illumination
+    tbout.meta["min_moon_separation"] = min_moon_separation
+    tbout.meta["min_altitude"] = min_alt
+    tbout.meta["max_altitude"] = max_alt
 
     if outfile == sys.stdout:
-        print(dfout)
+        print(tbout)
     else:
-        dfout.to_csv(outfile, index=True)
+        tbout.write(
+            outfile,
+            # formats={"date": "%Y-%m-%d"},
+            overwrite=True,
+        )
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Calculate observable time for HSC")
+    parser = argparse.ArgumentParser(description="Calculate observable time")
 
-    parser.add_argument("observing_runs", help="Observing runs csv file")
+    # parser.add_argument("observing_runs", help="Observing runs csv file")
+    parser.add_argument(
+        "start_date", help="Start date of the observing run (e.g., 2024-01-01)"
+    )
+    parser.add_argument(
+        "end_date", help="End date of the observing run, inclusive (e.g., 2024-12-31)"
+    )
     parser.add_argument(
         "outfile", help="Output csv file", nargs="?", default=sys.stdout
     )
@@ -251,14 +339,22 @@ if __name__ == "__main__":
         default=4,
     )
     parser.add_argument(
+        "--min-moon-illumination",
+        nargs=3,
+        help="Min moon illumination (dark, gray, bright), default is [None, 0.25, 0.65]",
+        type=list,
+        default=[None, 0.25, 0.65],
+    )
+    parser.add_argument(
         "--max-moon-illumination",
-        help="Max moon illumination",
-        type=float,
-        default=0.25,
+        nargs=3,
+        help="Max moon illumination (dark, gray, bright), default is [0.25, 0.65, None]",
+        type=list,
+        default=[0.25, 0.65, None],
     )
     parser.add_argument(
         "--min-moon-separation",
-        help="Min moon separation",
+        help="Min moon separation in degrees, default is 30",
         type=float,
         default=30,
     )
@@ -266,9 +362,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     main(
-        args.observing_runs,
+        # args.observing_runs,
+        args.start_date,
+        args.end_date,
         args.outfile,
         args.nside,
+        min_moon_illumination=args.min_moon_illumination,
         max_moon_illumination=args.max_moon_illumination,
         min_moon_separation=args.min_moon_separation,
     )
