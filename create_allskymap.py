c26fd2e3808bea4046bd700fa12364472dbe3e34
Masato Onodera <masato.onodera@gmail.com>
2024-11-04 00:20:27 -1000

    update plotting script with the latest file format.

--- create_allskymap.py
+++ create_allskymap.py
@@ -2,6 +2,7 @@
 
 # %%
 import argparse
+import datetime
 import os
 from collections import OrderedDict
 from pprint import pprint
@@ -12,6 +13,7 @@ import healpy as hp
 import numpy as np
 import pandas as pd
 from astropy import units as u
+from astropy.table import Table
 from bokeh.embed import file_html
 from bokeh.io import output_file, output_notebook, show
 from bokeh.layouts import column, gridplot, row
@@ -20,8 +22,28 @@ from uranography.api import MollweideMap
 
 
 # %%
-def generate_plot(datadir, run_name=None, nside=32, notebook=True):
+def generate_plot(
+    infiles, outfile, start_date, end_date, run_name=None, vmin=0, vmax=650
+):
 
+    start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+    end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+
+    print(start_date, end_date)
+
+    tables = []
+
+    if np.all(
+        [infile.endswith(".ecsv.gz") or infile.endswith(".ecsv") for infile in infiles]
+    ):
+        for infile in infiles:
+            print(f"Reading {infile}")
+            tb = Table.read(infile, format="ascii.ecsv")
+            tables.append(tb)
+    else:
+        raise ValueError("Input file must be in ecsv.gz or ecsv format.")
+
+    nside = tables[0].meta["nside"]
     area = hp.nside2pixarea(nside, degrees=True) * u.deg**2  # in square degrees
 
     print(f"{area=}")
@@ -29,24 +51,26 @@ def generate_plot(datadir, run_name=None, nside=32, notebook=True):
     dict = {
         "files": OrderedDict(),
         "moonsep": [],
-        "moonphase": [],
-        "vmin": 0,
-        "vmax": 310,
+        "moonphase": ["dark", "gray", "bright"],
+        # "vmin": 0,
+        # "vmax": 310,
+        "vmin": vmin,
+        "vmax": vmax,
         # "cmap": cc.CET_CBL3,
         # "cmap": cc.CET_L8,
         "cmap": cc.bmy,
-        "title": f"{run_name.upper()} HSC Observable Time",
+        "title": f"{run_name.upper()} Observable Time by Moon Separation ({start_date.date()} - {end_date.date()})",
         "note": f"pixel size: {area.value:.2f} sq. deg.",
     }
+    # print(f"{len(tables)=}")
 
-    for moonphase in ["dark", "gray"]:
-        for moonsep in [30, 60, 90]:
-            dict["files"][f"moonsep{moonsep}_{moonphase}"] = os.path.join(
-                datadir,
-                f"observable_time_{run_name}_nside{nside}_moonsep{moonsep}_{moonphase}.csv",
-            )
-            dict["moonphase"].append(moonphase)
-            dict["moonsep"].append(moonsep)
+    # for moonphase in ["dark", "gray", "bright"]:
+    moonsep = [int(tb.meta["min_moon_separation"]) for tb in tables]
+    for i, moonsep in enumerate(moonsep):
+        # print(i, moonsep)
+        dict["files"][f"moonsep{moonsep}"] = infiles[i]
+        # dict["moonphase"].append(moonphase)
+        dict["moonsep"].append(moonsep)
 
     pprint(dict["files"])
 
@@ -74,49 +98,73 @@ def generate_plot(datadir, run_name=None, nside=32, notebook=True):
         # border_line_color="black",
         bar_line_color="black",
         major_tick_line_color="black",
-        title="Observable time (h)",
+        title=f"Observable time in dark time with the minimum moon separation {dict['moonsep'][0]}deg [h]",
         title_text_align="center",
     )
 
     plot.add_layout(color_bar, "below")
 
-    for i, tup in enumerate(dict["files"].items()):
-        k, v = tup
-        print(i, k, v)
+    for i in range(len(tables)):
+
+        df = tables[i].to_pandas()
+        df["date"] = pd.to_datetime(df["date"])
+
+        df_agg = (
+            df.loc[
+                (df["date"] >= start_date) & (df["date"] <= end_date),
+                ["ipix", "dark", "gray", "bright"],
+            ]
+            .groupby("ipix")
+            .sum()
+        )
+
+        # print(df_agg.head(50))
 
         if i == 0:
-            try:
-                df_main = pd.read_csv(v)
-            except FileNotFoundError:
-                print(f"File not found: {v}")
-                raise FileNotFoundError(f"File not found {v}")
 
             hpix_ds, hp_cmap, hp_glyph = sky.add_healpix(
-                df_main["time_observable"].to_numpy(), nside=nside, cmap=cmap
+                df_agg[dict["moonphase"][0]].to_numpy(), nside=nside, cmap=cmap
             )
+            hpix_ds.data[f"{dict['moonphase'][1]}_{dict['moonsep'][i]}"] = df_agg[
+                dict["moonphase"][1]
+            ].to_numpy()
+            hpix_ds.data[f"{dict['moonphase'][2]}_{dict['moonsep'][i]}"] = df_agg[
+                dict["moonphase"][2]
+            ].to_numpy()
 
             hover_tool = sky.add_hp_hovertool(coordinates=True, value=None)
             hover_tool.tooltips.append(
                 (
-                    f"T({dict['moonphase'][i]}, moonsep={dict['moonsep'][i]}deg) [h]",
+                    f"T({dict['moonphase'][0]}, {dict['moonsep'][i]}deg) [h]",
                     "@value{0.0}",
-                )
+                ),
             )
-        else:
-            try:
-                df_comp = pd.read_csv(v)
-            except FileNotFoundError:
-                print(f"File not found: {v}")
-                continue
-
-            hpix_ds.data[f"count_{k}"] = df_comp["time_observable"].to_numpy()
-
             hover_tool.tooltips.append(
                 (
-                    f"T({dict['moonphase'][i]}, moonsep={dict['moonsep'][i]}deg) [h]",
-                    f"@count_{k}{{0.0}}",
-                )
+                    f"T({dict['moonphase'][1]}, {dict['moonsep'][i]}deg) [h]",
+                    f"@{dict['moonphase'][1]}_{dict['moonsep'][i]}{{0.0}}",
+                ),
             )
+            hover_tool.tooltips.append(
+                (
+                    f"T({dict['moonphase'][2]}, {dict['moonsep'][i]}deg) [h]",
+                    f"@{dict['moonphase'][2]}_{dict['moonsep'][i]}{{0.0}}",
+                ),
+            )
+        else:
+            # pass
+            #
+            for i_mp in range(len(dict["moonphase"])):
+                hpix_ds.data[f"{dict['moonphase'][i_mp]}_{dict['moonsep'][i]}"] = (
+                    df_agg[dict["moonphase"][i_mp]].to_numpy()
+                )
+
+                hover_tool.tooltips.append(
+                    (
+                        f"T({dict['moonphase'][i_mp]}, {dict['moonsep'][i]}deg) [h]",
+                        f"@{dict['moonphase'][i_mp]}_{dict['moonsep'][i]}{{0.0}}",
+                    ),
+                )
 
     sky.add_graticules(
         graticule_kwargs={
@@ -147,68 +195,76 @@ def generate_plot(datadir, run_name=None, nside=32, notebook=True):
     plot.min_border_top = 96
     plot.min_border_bottom = 96
 
-    grid = column(plot)
+    div = Div(
+        text="""<font size='5'>
+        Definition for the moon phase:
+        </font><br />
+        <font size='3'>
+        Dark: Moon illumination less than 0.25<br />
+        Gray: Moon illumination between 0.25 and 0.65<br />
+        Bright: Moon illumination more than 0.65<br />
+        Note that the moon phase is different from <a href="https://www.naoj.org/Observing/Proposals/howto.html">the definition of Subaru Telescope</a>.
+        </font>
+""",
+        margin=(0, 0, 0, 48),
+        # min_border_left=48,
+        # min_border_top=96,
+        # min_border_bottom=96,
+    )
 
-    if notebook:
-        output_notebook()
-        show(grid, notebook_handle=True)
-    else:
-        output_file(
-            f"hsc_observable_time_{run_name}_nside{nside}.html", title=dict["title"]
-        )
+    grid = column(plot, div)
 
-    print(f"Saving to hsc_observable_time_{run_name}_nside{nside}.html")
+    output_file(outfile, title=dict["title"])
 
     saved_file = bokeh.plotting.save(grid)
 
 
 # %%
 if __name__ == "__main__":
-    try:
-        get_ipython
-        print("running in a notebook")
+    parser = argparse.ArgumentParser(description="Create plot")
 
-        generate_plot(
-            "output2",
-            "hsc_observable_time_s24a_nside32.html",
-            run_name="s24a",
-            nside=32,
-            notebook=True,
-        )
-        generate_plot(
-            "output2",
-            "hsc_observable_time_s24b_nside32.html",
-            run_name="s24b",
-            nside=32,
-            notebook=True,
-        )
-    except NameError:
+    parser.add_argument(
+        "input_files",
+        nargs="+",
+        type=str,
+        help="Input files (ecsv or ecsv.gz)",
+    )
+    parser.add_argument("output_file", type=str, help="Output file (html)")
+    parser.add_argument(
+        "start_date",
+        type=str,
+        help="Start date (YYYY-MM-DD)",
+    )
+    parser.add_argument(
+        "end_date",
+        type=str,
+        help="End date (YYYY-MM-DD)",
+    )
+    parser.add_argument(
+        "--run-name", type=str, default="s24a", help="Run name (default: s24a)"
+    )
 
-        parser = argparse.ArgumentParser(description="Create plot")
+    parser.add_argument(
+        "--vmin", type=int, default=0, help="Minimum value for colorbar"
+    )
+    parser.add_argument(
+        "--vmax", type=int, default=650, help="Maximum value for colorbar"
+    )
 
-        parser.add_argument(
-            "datadir",
-            type=str,
-            help="Data directory. Filenames must follow the pattern: observable_time_{{run_name}}_nside{{nside}}_moonsep{{moonsep}}_{{moonphase}}.csv",
-        )
-        # parser.add_argument("outfile", type=str, help="Output file")
-        parser.add_argument(
-            "--run_name", type=str, default="s24a", help="Run name (default: s24a)"
-        )
-        parser.add_argument(
-            "--nside", type=int, default=32, help="Healpix nside (default: 32)"
-        )
-        parser.add_argument("--notebook", action="store_true", help="Notebook mode")
+    args, unknown = parser.parse_known_args()
 
-        args, unknown = parser.parse_known_args()
+    print(args)
 
-        print(f"running with args: {args}")
-        generate_plot(
-            args.datadir,
-            run_name=args.run_name,
-            nside=args.nside,
-            notebook=args.notebook,
-        )
+    print(f"running with args: {args}")
+    generate_plot(
+        args.input_files,
+        args.output_file,
+        args.start_date,
+        args.end_date,
+        run_name=args.run_name,
+        vmin=args.vmin,
+        vmax=args.vmax,
+    )
 
 
 # %%
