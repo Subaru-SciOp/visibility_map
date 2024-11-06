35d38de4a888f994b5be5a59de3a997ce4ff9472
Masato Onodera <monodera@naoj.org>
2024-11-05 18:53:37 -1000

    modify shell script for S25AB semesters

--- run_calculations.bash
+++ run_calculations.bash
@@ -2,8 +2,11 @@
 
 NSIDE=32
 DATADIR="data"
-START_DATE="2024-02-01"
-END_DATE="2025-01-31"
+# START_DATE="2024-02-01"
+# END_DATE="2025-01-31"
+
+START_DATE="2025-02-01"
+END_DATE="2026-01-31"
 
 for moonsep in 30 60 90 45; do
     time python3 -u ./calculate_observable_time.py $START_DATE $END_DATE ${DATADIR}/observable_time_nside${NSIDE}_moonsep${moonsep}_${START_DATE//-/}-${END_DATE//-/}.parquet \
